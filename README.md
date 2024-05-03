a4a4ac509487a98a1e0468b54875a56687002109
Stefano Fancello <gentoo.stefano@gmail.com>
2024-05-03 20:07:25 +0200

    Load tickers from command line or environment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FinanceAlerts
 
-For each ticker in `config/tickers.txt` check RSI and bollinger bands and output a BUY/SELL signal. 
+For each ticker in environment var TICKER_LIST, the software checks RSI and bollinger bands and output a BUY/SELL signal. 
 
 If a Telegram TELEGRAM_CHAT_ID and TELEGRAM_BOT_TOKEN environment variables are provided, alerts are also sent to the Telegram chat
 
@@ -13,15 +13,47 @@ TELEGRAM_BOT_TOKEN=1234567890:XXX-XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxX
 ## Launch:
 
 ```
+export TICKER_LIST="TSLA INTC"
 python3 src/main.py
+```
+
+or
+
+```
+python3 src/main.py INTC
 ```
 
 ## How to setup your github action bot:
 
-- fork this repository
 - add into your https://github.com/YOUR_USERNAME/financealerts/settings/secrets/actions the following variables:
 `DOCKERHUB_USERNAME`: your [dockerhub](https://hub.docker.com/) username
 `DOCKERHUB_TOKEN`: dockerhub read/write access token https://hub.docker.com/settings/security
 `TELEGRAM_CHAT_ID`: the [ID of the telegram chat](https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id) you whant your message to be sent
 `TELEGRAM_BOT_TOKEN`: a telegram bot [token](https://core.telegram.org/bots/features#botfather)
+`TICKER_LIST`
 - change all "stell0" occurrence in .github/workflows/*.yml with your username
+
+
+- fork this repository
+
+> **_NOTE:_**  go to the Actions tab and enable the scheduled workflows because they are disabled for the forked repositories.
+
+- add into your [action secrets](https://github.com/YOUR_USERNAME/financealerts/settings/secrets/actions)
+ the following variables:
+
+`TELEGRAM_CHAT_ID`: the [ID of the telegram chat](https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id) you whant your message to be sent
+
+`TELEGRAM_BOT_TOKEN`: a telegram bot [token](https://core.telegram.org/bots/features#botfather)
+
+- Then add in your [action variables](https://github.com/YOUR_USERNAME/financealerts/settings/variables/actions)the variable with the list of tickers to analyze
+
+`TICKER_LIST`: list of tickers
+```
+TSLA
+INTC
+AAPL
+```
+
+**_NOTE:_** you can add ticker from London stock exchange adding a .L to the ticker, and .AX for Australian stock exchange e.g. ANIC.L, PLS.AX
+
+then wait ⏱️  (cron is launched once a day)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import yahoo_finance_api
 import indicators
 import telegram_alert
 import signals
-import os
+import os, sys
 import datetime
 import re
 
@@ -12,44 +12,55 @@ def load_tickers(file_path):
     return tickers
 
 def main():
-    tickers = load_tickers(os.path.abspath(os.path.dirname(__file__))+'/../config/tickers.txt')
-    report = {}
+    # load ticker from argv if available
+    if len(sys.argv) > 1:
+        tickers = [sys.argv[1]]
+    # load tickers from environment
+    else:
+        ticker_list = os.environ.get("TICKER_LIST")
+        tickers = ticker_list.split()
 
+    report = {}
     # initialize signal column with 0
     for ticker in tickers:
-        print(ticker)
-        df = yahoo_finance_api.retrieve_daily_prices(ticker,period=365)
-        df = indicators.rsi(df)
-        df = indicators.sma(df, timeperiod=200, label="SMA200")
-        df = indicators.linearreg(df)
-        df = indicators.sma(df, timeperiod=40, label="SMA50")
-        df = indicators.macd(df)
-        df = indicators.bb(df)
+        try:
+            print(ticker)
+            df = yahoo_finance_api.retrieve_daily_prices(ticker,period=365)
+            df = indicators.rsi(df)
+            df = indicators.sma(df, timeperiod=200, label="SMA200")
+            df = indicators.linearreg(df)
+            df = indicators.sma(df, timeperiod=40, label="SMA50")
+            df = indicators.macd(df)
+            df = indicators.bb(df)
         
-        df['signal'] = 0
-        df['reason'] = ""
-        df = signals.rsi(df)
-        #df = signals.sma(df,label="SMA200")
-        #df = signals.movingAverageCrossover(df,short='SMA50',long='SMA200')
-        #df = signals.macd(df)
-        df = signals.bb(df)
+            df['signal'] = 0
+            df['reason'] = ""
+            df = signals.rsi(df)
+            #df = signals.sma(df,label="SMA200")
+            #df = signals.movingAverageCrossover(df,short='SMA50',long='SMA200')
+            #df = signals.macd(df)
+            df = signals.bb(df)
        
-        # don't send aler if signal date is not today
-        if df.index[-1].strftime('%Y-%m-%d') != datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d'):
-            continue
+            # don't send aler if signal date is not today
+            if df.index[-1].strftime('%Y-%m-%d') != datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d'):
+                continue
 
-        # send telegram alert if last signal is not 0
-        if df.iloc[-1]['signal'] != 0:
-            if df.iloc[-1]['signal'] > 0:
-                signal = "BUY"
-            if df.iloc[-1]['signal'] < 0:
-                signal = "SELL"
-            if not signal in report:
-                report[signal] = {}
+            # send telegram alert if last signal is not 0
+            if df.iloc[-1]['signal'] != 0:
+                if df.iloc[-1]['signal'] > 0:
+                    signal = "BUY"
+                if df.iloc[-1]['signal'] < 0:
+                    signal = "SELL"
+                if not signal in report:
+                    report[signal] = {}
             
-            #remove trailing comma from reason
-            reason = re.sub (r",$", "", df.iloc[-1]['reason'])
-            report[signal][ticker] = {"signal":signal,"strength":df.iloc[-1]['signal'],"reason":reason}
+                #remove trailing comma from reason
+                reason = re.sub (r",$", "", df.iloc[-1]['reason'])
+                report[signal][ticker] = {"signal":signal,"strength":df.iloc[-1]['signal'],"reason":reason}
+
+        except Exception as e:
+            print(e)
+            continue
 
     # create the final report
     report_text = ""
@@ -68,8 +79,6 @@ def main():
 
     if report_text != "":
         telegram_alert.send_alert(report_text)
-
-
 
 if __name__ == "__main__":
     main()
